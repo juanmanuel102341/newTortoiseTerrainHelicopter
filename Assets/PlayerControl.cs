b2cb77a542f52dcc91b6b7dd4fd2e15c60eb2964
juanmanuel102341 <jms24@live.com.ar>
2017-07-07 13:00:29 -0300

    viene d desktop hit hub, proyecto base terreno y helicoptero

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float velocity;
+	// Use this for initialization
+	void Start () {
+		rb=GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+
+			rb.transform.Rotate(Vector3.up*-velocity*Time.deltaTime);
+		}else if(Input.GetKey(KeyCode.D)){
+			rb.transform.Rotate(Vector3.up*velocity*Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.W)){
+			rb.transform.Translate(Vector3.back*velocity*Time.deltaTime);
+
+		}else if(Input.GetKey(KeyCode.S)){
+			rb.transform.Translate(Vector3.forward*velocity*Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Rotacion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour {
+	public float velocity;
+	private Transform tf;
+	// Use this for initialization
+	void Start () {
+		tf=GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tf.transform.Rotate(Vector3.forward*velocity*Time.deltaTime);	
+	}
+}
